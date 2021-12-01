6c003007b3e70493822360dab9dc6b4f4297c932
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-01 23:47:19 +0900

    clear movie api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
   //데코레이터라고 부른다 데코레이터는 클래스에 함수 기능을 추가할 수 있다.
-  imports: [],
-  controllers: [MoviesController], //url을 가져오고 함수를 실행시킨다. express라우터 같은것
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], //url을 가져오고 함수를 실행시킨다. express라우터 같은것
+  providers: [],
 })
 export class AppModule {} //모든것의 루트 모듈
