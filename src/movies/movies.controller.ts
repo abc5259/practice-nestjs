811f6eccab3cf94a2d50f4628561b035b31c3459
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-01 22:43:59 +0900

    practive DTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -7,11 +7,13 @@ import {
   Patch,
   Body,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies') //movies라우터링 같음
 export class MoviesController {
+  //매개변수 프로퍼티(Parameter Property) 를 이용하여 선언과 할당을 동시에 할 수 있습니다
   constructor(private readonly moviesService: MoviesService) {}
   @Get()
   getAll(): Movie[] {
@@ -19,25 +21,23 @@ export class MoviesController {
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string): boolean {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  path(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updataeMovie: movieId,
-      ...updateData,
-    };
+  path(@Param('id') movieId: number, @Body() updateData) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
